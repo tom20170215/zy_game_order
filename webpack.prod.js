d3fc3e2f95c94055a8337434c924836b76e35663
tom20170215 <tom_tyceshi@163.com>
2018-08-06 15:07:55 +0800

    webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,5 +12,30 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
-    ]
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'all',
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendor",
+                    minChunks: 1,
+                    maxInitialRequests: 5,
+                    priority: 100
+                },
+                common: {
+                    test: /[\\/]src[\\/]js[\\/]/,
+                    name: 'common',
+                    chunks: 'all',
+                    minChunks: 2,
+                    maxInitialRequests: 5,
+                    priority: 1
+                }
+            }
+        },
+        runtimeChunk: {
+            name: 'manifest'
+        }
+    }
 });
